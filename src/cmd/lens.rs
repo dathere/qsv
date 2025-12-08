ce25ce9995ecd64b0b40cb1d3c309f624cb9f927
Joel Natividad <1980690+jqnatividad@users.noreply.github.com>
2025-12-07 20:54:41 -0500

    feat: `lens` add `--streaming-stdin` option

--- src/cmd/lens.rs
+++ src/cmd/lens.rs
@@ -87,6 +87,9 @@ lens options:
                                      qsv lens -W w data.csv // wrap at word boundaries
                                    [default: disabled]
   -A, --auto-reload                Automatically reload the data when the file changes.
+  --streaming-stdin                Stream stdin instead of loading the entire input before displaying.
+                                   NOTE: This option only applies to stdin input.
+  
 
   -P. --prompt <prompt>            Set a custom prompt in the status bar. Normally paired w/ --echo-column:
                                      qsv lens --prompt 'Select City:' --echo-column 'City'
@@ -119,40 +122,49 @@ use crate::{CliError, CliResult, config::Config, util};
 
 #[derive(Deserialize)]
 struct Args {
-    arg_input:           Option<String>,
-    flag_delimiter:      Option<String>,
-    flag_tab_separated:  bool,
-    flag_no_headers:     bool,
-    flag_columns:        Option<String>,
-    flag_filter:         Option<String>,
-    flag_find:           Option<String>,
-    flag_ignore_case:    bool,
-    flag_freeze_columns: Option<u64>,
-    flag_monochrome:     bool,
-    flag_prompt:         Option<String>,
-    flag_echo_column:    Option<String>,
-    flag_wrap_mode:      String,
-    flag_auto_reload:    bool,
-    flag_debug:          bool,
+    arg_input:            Option<String>,
+    flag_delimiter:       Option<String>,
+    flag_tab_separated:   bool,
+    flag_no_headers:      bool,
+    flag_columns:         Option<String>,
+    flag_filter:          Option<String>,
+    flag_find:            Option<String>,
+    flag_ignore_case:     bool,
+    flag_freeze_columns:  Option<u64>,
+    flag_monochrome:      bool,
+    flag_prompt:          Option<String>,
+    flag_echo_column:     Option<String>,
+    flag_wrap_mode:       String,
+    flag_auto_reload:     bool,
+    flag_debug:           bool,
+    flag_streaming_stdin: bool,
 }
 
 pub fn run(argv: &[&str]) -> CliResult<()> {
     let args: Args = util::get_args(USAGE, argv)?;
 
-    // Process input file
-    // support stdin and auto-decompress snappy file
-    // stdin/decompressed file is written to a temporary file in tmpdir
-    // which is automatically deleted after the command finishes
+    if args.flag_streaming_stdin && args.arg_input.is_some() {
+        return fail_incorrectusage_clierror!("--streaming-stdin only applies to stdin input.");
+    }
+
     let tmpdir = tempfile::tempdir()?;
-    let work_input = util::process_input(
-        vec![PathBuf::from(
-            // if no input file is specified, read from stdin "-"
-            args.arg_input.unwrap_or_else(|| "-".to_string()),
-        )],
-        &tmpdir,
-        "",
-    )?;
-    let input = work_input[0].to_string_lossy().to_string();
+    let input = if args.flag_streaming_stdin && args.arg_input.is_none() {
+        "-".to_string()
+    } else {
+        // Process input file
+        // support stdin and auto-decompress snappy file
+        // stdin/decompressed file is written to a temporary file in tmpdir
+        // which is automatically deleted after the command finishes
+        let work_input = util::process_input(
+            vec![PathBuf::from(
+                // if no input file is specified, read from stdin "-"
+                args.arg_input.clone().unwrap_or_else(|| "-".to_string()),
+            )],
+            &tmpdir,
+            "",
+        )?;
+        work_input[0].to_string_lossy().to_string()
+    };
 
     // If the prompt starts with "file:", it's interpreted as a filepath
     // from which to load the prompt, e.g.
@@ -216,6 +228,7 @@ pub fn run(argv: &[&str]) -> CliResult<()> {
         prompt,
         wrap_mode,
         auto_reload: args.flag_auto_reload,
+        no_streaming_stdin: !args.flag_streaming_stdin,
     };
 
     let out = run_csvlens_with_options(options)
